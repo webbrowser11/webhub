483dc97158c9788323192410d81f2b91c75fc6b4
webbrowser11 <terpstragraham@gmail.com>
2024-05-24 16:38:34 -0700

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-function setLanguage(language) {
-    const elements = document.querySelectorAll('[data-en]');
-
-    elements.forEach(element => {
-        element.innerHTML = element.getAttribute(`data-${language}`);
-    });
-}
